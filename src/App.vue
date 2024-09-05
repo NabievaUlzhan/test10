2fe424681bcc1798d9075adc8e61bba3dccabdc9
NabievaUlzhan <152633849+NabievaUlzhan@users.noreply.github.com>
2024-09-05 21:03:19 +0500

    Создание проекта

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <todoList></todoList>
+  <usersList></usersList>
+</template>
+
+<script setup>
+  import todoList from './components/todoList.vue'
+  import usersList from './components/usersList.vue'
+</script>
+
+<style>
+
+</style>
